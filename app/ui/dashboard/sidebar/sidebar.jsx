339f9ddefdeea2e3269fc319c62ef061d7479764
unknown <fatahillah955@gmail.com>
2024-06-05 15:24:24 +0800

    commit2

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import styles from "./sidebar.module.css"
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttackMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+}  from "react-icons/md";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path :"/dashboard/users",
+        icon : <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag /> ,
+      },
+      {
+        title: "Transactions"
+      }
+    ]
+  }
+]
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className={styles.container}>
+      <div>
+        <h1>SiVena</h1>
+      </div>
+    </div>
   )
 }
 
